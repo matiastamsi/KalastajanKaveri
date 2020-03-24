12db87cb72e466a67be6183aaee2621597ac98f7
Matias Tamsi <matias.tamsi@gmail.com>
2020-03-24 11:47:25 +0200

    Added a sign-up function to new users

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
 from application.auth.forms import LoginForm
 
@@ -19,10 +19,23 @@ def auth_login():
                                error = "No such username or password")
 
 
-    print("User " + user.name + " recognized")
+    login_user(user)
     return redirect(url_for("index"))
 
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/auth/new/")
+def auth_form():
+    return render_template("auth/new.html")
+
+@app.route("/auth/", methods=["POST"])
+def auth_create():
+    u = User(request.form.get("name"), request.form.get("username"), request.form.get("password"))
+
+    db.session().add(u)
+    db.session().commit()
+
     return redirect(url_for("index"))
