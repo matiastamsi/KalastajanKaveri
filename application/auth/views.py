d8e8cf41c99b8bb1fa8aa118784210c42ccc9fa2
Matias Tamsi <matias.tamsi@gmail.com>
2020-03-24 10:20:25 +0200

    Concrete sign functions added

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for
+from flask_login import login_user
 
 from application import app
 from application.auth.models import User
@@ -19,4 +20,9 @@ def auth_login():
 
 
     print("User " + user.name + " recognized")
+    return redirect(url_for("index"))
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
     return redirect(url_for("index"))
