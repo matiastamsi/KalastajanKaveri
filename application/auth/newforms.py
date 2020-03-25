b65b2d43719ed41549357d2757dad8d0cc728afe
Matias Tamsi <matias.tamsi@gmail.com>
2020-03-25 15:26:25 +0200

    Removed old comment

--- application/auth/newforms.py
+++ application/auth/newforms.py
@@ -3,7 +3,7 @@ from wtforms import PasswordField, StringField, validators
 
 class SignUpForm(FlaskForm):
     name = StringField('Name', [validators.Length(min=2)])
-    username = StringField('Username', [validators.Length(min=8)]) #Pitää lisätä validointi uniikkiudesta
+    username = StringField('Username', [validators.Length(min=8)])
     password = PasswordField('New Password', [validators.Length(min=10), validators.EqualTo('confirm')])
     confirm  = PasswordField('Repeat Password')
 
