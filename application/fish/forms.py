31615d96b75af6d237d04d95e761f7e46f8e8bbf
Matias Tamsi <matias.tamsi@gmail.com>
2020-04-02 22:46:35 +0300

    There were missing initialazing text in Add a new species form. Now it is clear what are those days and months representing.

--- application/fish/forms.py
+++ application/fish/forms.py
@@ -5,9 +5,9 @@ class FishForm(FlaskForm):
 
     name = StringField("Species")
     minimum_catch_size = DecimalField("Minimum size to keep", [validators.NumberRange(min=0.1, max=150.0)], places=2)
-    closed_season_starts_day = IntegerField("Day", [validators.NumberRange(min=1, max=31)])
+    closed_season_starts_day = IntegerField("Closed season starts: Day", [validators.NumberRange(min=1, max=31)])
     closed_season_starts_month = IntegerField("Month", [validators.NumberRange(min=1, max=12)])
-    closed_season_ends_day = IntegerField("Day", [validators.NumberRange(min=1, max=31)])
+    closed_season_ends_day = IntegerField("Closed season ends: Day", [validators.NumberRange(min=1, max=31)])
     closed_season_ends_month = IntegerField("Month", [validators.NumberRange(min=1, max=12)])
     
     delete = BooleanField("Delete the species permanently")
