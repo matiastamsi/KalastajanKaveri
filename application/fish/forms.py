6dab7f7a93f3714b64d43cd9764eeee032be3f5d
Matias Tamsi <matias.tamsi@gmail.com>
2020-04-02 22:33:47 +0300

    Fixed mistake in validators value. Updated user stories.

--- application/fish/forms.py
+++ application/fish/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, DecimalField, IntegerField, BooleanField, valid
 class FishForm(FlaskForm):
 
     name = StringField("Species")
-    minimum_catch_size = DecimalField("Minimum size to keep", [validators.NumberRange(min=0.1, max=50.0)], places=2)
+    minimum_catch_size = DecimalField("Minimum size to keep", [validators.NumberRange(min=0.1, max=150.0)], places=2)
     closed_season_starts_day = IntegerField("Day", [validators.NumberRange(min=1, max=31)])
     closed_season_starts_month = IntegerField("Month", [validators.NumberRange(min=1, max=12)])
     closed_season_ends_day = IntegerField("Day", [validators.NumberRange(min=1, max=31)])
